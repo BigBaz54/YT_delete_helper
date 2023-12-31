9ca2735831bea3f3073d61ae6223baca627a6e4a
Louis-Vincent CAPELLI <96493391+BigBaz54@users.noreply.github.com>
2023-12-30 20:57:11 -0500

    [+] Waits for loading and launches afterwards

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         YT delete helper
 // @namespace    http://tampermonkey.net/
-// @version      1.3
+// @version      2.0
 // @description  YT delete helper for playlists
 // @author       BigBaz
 // @match        https://www.youtube.com/*
@@ -9,28 +9,42 @@
 // @grant        none
 // ==/UserScript==
 
-(async function() {
+(function() {
     'use strict';
 
-    for (var i=0; i<document.querySelectorAll("#menu.ytd-playlist-video-renderer.style-scope").length;i++) {
-        document.querySelectorAll("#menu.ytd-playlist-video-renderer.style-scope")[i].setAttribute("n", i)
+    async function create_buttons() {
+        for (var i=0; i<document.querySelectorAll("#menu.ytd-playlist-video-renderer.style-scope").length;i++) {
+            document.querySelectorAll("#menu.ytd-playlist-video-renderer.style-scope")[i].setAttribute("n", i)
+        }
+
+        document.querySelectorAll("#menu.ytd-playlist-video-renderer.style-scope").forEach(
+            async (e) => {
+                const n = e.getAttribute('n');
+                e.appendChild(e.children[0].cloneNode());
+                const buttons = document.querySelectorAll("[n='"+n.toString()+"']>ytd-menu-renderer>yt-icon-button>#button");
+                const newButton = buttons[1];
+                await new Promise(r => setTimeout(r, 500));
+                document.querySelectorAll("[n='"+n.toString()+"']>ytd-menu-renderer>yt-icon-button>#button>yt-icon>yt-icon-shape>icon-shape>div>svg>path")[1].setAttribute("d", "M23.6464 0.646446L0.646447 23.6464M0.353554 0.646446L23.3536 23.6464");
+                document.querySelectorAll("[n='"+n.toString()+"']>ytd-menu-renderer>yt-icon-button>#button>yt-icon>yt-icon-shape>icon-shape>div>svg>path")[1].setAttribute("stroke", "white");
+                newButton.onclick = async () => {
+                    document.querySelector("[n='"+n.toString()+"']>ytd-menu-renderer>yt-icon-button>#button").click();
+                    document.querySelector("[n='"+n.toString()+"']>ytd-menu-renderer>yt-icon-button>#button").click();
+                    await new Promise(r => setTimeout(r, 100));
+                    document.querySelector("#items > ytd-menu-service-item-renderer:nth-child(4)").click();
+                }
+            }
+        )
     }
 
-    document.querySelectorAll("#menu.ytd-playlist-video-renderer.style-scope").forEach(
-        async (e) => {
-            const n = e.getAttribute('n');
-            e.appendChild(e.children[0].cloneNode());
-            const buttons = document.querySelectorAll("[n='"+n.toString()+"']>ytd-menu-renderer>yt-icon-button>#button");
-            const newButton = buttons[1];
-            await new Promise(r => setTimeout(r, 500));
-            document.querySelectorAll("[n='"+n.toString()+"']>ytd-menu-renderer>yt-icon-button>#button>yt-icon>yt-icon-shape>icon-shape>div>svg>path")[1].setAttribute("d", "M23.6464 0.646446L0.646447 23.6464M0.353554 0.646446L23.3536 23.6464");
-            document.querySelectorAll("[n='"+n.toString()+"']>ytd-menu-renderer>yt-icon-button>#button>yt-icon>yt-icon-shape>icon-shape>div>svg>path")[1].setAttribute("stroke", "white");
-            newButton.onclick = async () => {
-                document.querySelector("[n='"+n.toString()+"']>ytd-menu-renderer>yt-icon-button>#button").click();
-                document.querySelector("[n='"+n.toString()+"']>ytd-menu-renderer>yt-icon-button>#button").click();
-                await new Promise(r => setTimeout(r, 100));
-                document.querySelector("#items > ytd-menu-service-item-renderer:nth-child(4)").click();
-            }
+    async function wait_loading() {
+        if (document.querySelectorAll('.ytd-playlist-video-list-renderer').length > 50) {
+            await new Promise(r => setTimeout(r, 2000));
+            create_buttons()
+        } else {
+            console.log('Waiting for loading');
+            setTimeout(wait_loading, 500)
         }
-    )
+    }
+
+    wait_loading()
 })();
